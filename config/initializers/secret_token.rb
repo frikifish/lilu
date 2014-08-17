c19a0852ccf530149a8133b2f1f94326280625d8
frikifish <frikifish@gmail.com>
2014-08-16 23:14:09 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lilu::Application.config.secret_key_base = '435960c9ed5e6d5a105527ebde3c302965ff7e17607d48538fa5b6a0311707e63904734071a902336535e3148fb61798a729c608384f11326d5e1b34b876de18'
